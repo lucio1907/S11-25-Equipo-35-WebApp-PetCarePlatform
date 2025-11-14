9ab85684916f879a85eaf16866a53c2b630a5461
jooseariias <arijoose@gmail.com>
2025-11-14 17:42:41 -0300

    actualiza el mensaje de bienvenida y unifica el estilo de comillas en App.js

--- client/App.js
+++ client/App.js
@@ -1,16 +1,20 @@
-import Register from "./src/Screens/Register";
-import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Register />
+      <Text>Hello world</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-};
+});
