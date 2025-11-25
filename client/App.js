d86ec4720bf8ffa2c5ca3334dbc763d40c1cc7d9
jooseariias <arijoose@gmail.com>
2025-11-25 11:42:11 -0300

    "navegacion " y alertas

--- client/App.js
+++ client/App.js
@@ -1,16 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootStack from "./src/Navigation/RootStack";
-import Toast from "react-native-toast-message";
-import { toastConfig } from "./src/Config/toastConfig";
-import { AuthProvider } from "./src/Context/AuthContext";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
-      <Toast config={toastConfig} />
-    </AuthProvider>
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
   );
 }
