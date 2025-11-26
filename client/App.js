139eb1362db2cfd81efcf3f435f98873cfcc1624
jooseariias <arijoose@gmail.com>
2025-11-26 13:45:37 -0300

    feat(Login, Register and Alerts): :sparkles: ya funciona el registro y el login se guarda tonken en store

--- client/App.js
+++ client/App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootStack from "./src/Navigation/RootStack";
+import Toast from "react-native-toast-message";
+import { toastConfig } from "./src/Config/toastConfig";
 
 export default function App() {
   return (
