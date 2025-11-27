1d16d4a8738eb8d3aefbc86d9fec0ccb0205077c
jooseariias <arijoose@gmail.com>
2025-11-27 13:00:14 -0300

    "corecion de login ,login permanente y hook para usar el token en rutas"

--- client/src/Screens/Homee.js
+++ client/src/Screens/Homee.js
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+Button;
+import { useAuth } from "../Context/AuthContext";
 
 export default function Homee() {
+  const { logout } = useAuth();
   return (
-    <View>
-      <Text>Hola esto es solo de prueba</Text>
+    <View style={{ flex: 1, marginTop: 20 }}>
+      <Text>home</Text>
+      <Button title="cerrar" onPress={() => logout()} />
     </View>
   );
 }
