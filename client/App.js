dde7840ee1e9de5132ac839dbc0c4ce491736cd3
jooseariias <arijoose@gmail.com>
2025-11-27 13:00:14 -0300

    "corecion de login ,login permanente y hook para usar el token en rutas"

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import RootStack from "./src/Navigation/RootStack";
 import Toast from "react-native-toast-message";
 import { toastConfig } from "./src/Config/toastConfig";
+import { AuthProvider } from "./src/Context/AuthContext";
 
 export default function App() {
   return (
