113a2b654f3a13decf8eba0addf0104b8a14a659
jooseariias <arijoose@gmail.com>
2025-11-27 13:00:14 -0300

    "corecion de login ,login permanente y hook para usar el token en rutas"

--- client/src/Navigation/RootStack.js
+++ client/src/Navigation/RootStack.js
@@ -1,17 +1,28 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useAuth } from "../Context/AuthContext";
 
 import Login from "../Screens/Login";
 import Register from "../Screens/Register";
 import Home from "../Screens/Homee";
+import Loading from "../Components/Loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootStack() {
+  const { token, loading } = useAuth();
+
+  if (loading) return <Loading />;
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} headerShown={false} />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="Home" component={Home} />
+      {token ? (
+        <Stack.Screen name="Home" component={Home} />
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
