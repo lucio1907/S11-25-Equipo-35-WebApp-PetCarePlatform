0c2322138662698a1cc62308f68e6f82d1c1d0fe
lucio1907 <luciogastellu.dev@gmail.com>
2025-11-27 09:44:11 -0300

    feat: implement Medication service and mapper for medication management

--- server/src/main/java/com/pethealthtracker/model/Vaccination.java
+++ server/src/main/java/com/pethealthtracker/model/Vaccination.java
@@ -2,7 +2,6 @@ package com.pethealthtracker.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
