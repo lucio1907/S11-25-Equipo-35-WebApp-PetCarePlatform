c65ef940191292a610bafa2d70d47e70d70de0d2
jooseariias <arijoose@gmail.com>
2025-12-01 19:19:00 -0300

    "layaud de las patitas para varias vistas"

--- client/src/Screens/Homee.js
+++ client/src/Screens/Homee.js
@@ -1,13 +1,17 @@
 import { View, Text, Button } from "react-native";
-Button;
+
 import { useAuth } from "../Context/AuthContext";
+
+import Layout from "../Components/Layout";
 
 export default function Homee() {
   const { logout } = useAuth();
   return (
-    <View style={{ flex: 1, marginTop: 20 }}>
-      <Text>home</Text>
-      <Button title="cerrar" onPress={() => logout()} />
-    </View>
+    <Layout>
+      <View style={{ flex: 1, marginTop: 20 }}>
+        <Text>home</Text>
+        <Button title="cerrar" onPress={() => logout()} />
+      </View>
+    </Layout>
   );
 }
