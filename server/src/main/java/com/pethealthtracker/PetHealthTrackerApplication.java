e4740c03ba628fe46a827b65ed56bd3b9f25f480
lucio1907 <luciogastellu.dev@gmail.com>
2025-12-01 10:09:52 -0300

    feat: implement email verification process with HTML responses and email service integration

--- server/src/main/java/com/pethealthtracker/PetHealthTrackerApplication.java
+++ server/src/main/java/com/pethealthtracker/PetHealthTrackerApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
+@EnableAsync
 public class PetHealthTrackerApplication {
 
     @Autowired
